2c9f16814a5db899bdbfe9d8e141653eb036f994
Maureen Roble <moroble@gmail.com>
2015-06-08 12:06:48 -0400

    Add search to address book model

--- models/address_book.rb
+++ models/address_book.rb
@@ -34,6 +34,27 @@ require "csv"
 
       return csv.count
     end
+
+    def binary_search(name)
+       lower = 0
+     upper = entries.length - 1
+
+     while lower <= upper
+
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+     return nil
    end
+
 
 
